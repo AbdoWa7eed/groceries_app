dc7e5ad2b81ed251f0c8431886efbb0098b30b2b
AbdoWa7eed <abdo20132233@gmail.com>
2024-07-14 20:00:27 +0300

    Added Network Files For APIs - Added exception handling files - Added necessary packages for APIs requests

---
+++ lib/core/network/dio_factory.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:groceries_app/core/network/api_constants.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const String _applicationJson = "application/json";
+
+abstract class DioFactory {
+  static Dio getDio() {
+    Dio dio = Dio();
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: _applicationJson,
+      HttpHeaders.acceptHeader: _applicationJson,
+    };
+    dio.options = BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        headers: headers,
+        receiveTimeout: ApiConstants.apiTimeOut,
+        sendTimeout: ApiConstants.apiTimeOut);
+
+    dio.interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        requestHeader: true,
+        responseHeader: true,
+      ),
+    );
+    return dio;
+  }
+
+}
